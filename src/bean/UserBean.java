9b7e7ad7bb37ab5cec0d31afe05a6eb828fe733e
konstantinoscs <kons.ste@gmail.com>
2017-08-22 21:38:17 +0300

    bean and new gitignore

---
+++ src/bean/UserBean.java
@@ -0,0 +1,37 @@
+package bean;
+
+import model.User;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.Column;
+
+@ManagedBean(name="user")
+@SessionScoped
+public class UserBean {
+	private int id;
+	private String email;
+	private String firstName;
+	private byte isHost;
+	private byte isVerified;
+	private String lastName;
+	private String passhash;
+	private String phoneNum;
+	private String username;
+	
+	public void registerUser()
+	{
+		User user = new User();
+		
+		user.setFirstName(firstName);
+		user.setEmail(email);
+		user.setIsHost(isHost);
+		user.setIsVerified(isVerified);
+		user.setLastName(lastName);
+		user.setPhoneNum(phoneNum);
+		user.setUsername(username);
+		//dao goes here
+	
+	}
+
+}
