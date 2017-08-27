6363ad05fcd9693ddf746bd2768602802f231fb0
Giannis Lam <john-lamboff@outlook.com>
2017-08-27 18:37:05 +0300

    bean

---
+++ src/bean/MainSearchBean.java
@@ -0,0 +1,24 @@
+package bean;
+
+import model.Listing;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.Column;
+
+@ManagedBean(name="main_search")
+@SessionScoped
+public class MainSearchBean {
+
+  private String search_str;
+  
+  public void set_search_str(String input_str) {
+    this.search_str = input_str;
+  }
+  
+  public String get_search_str() {
+    return search_str;
+  }
+  
+  
+} 
