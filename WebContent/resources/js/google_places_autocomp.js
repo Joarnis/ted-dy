4912cce7f2eb3f4b363c8d40979440240dd61f15
Giannis Lam <john-lamboff@outlook.com>
2017-08-29 03:07:58 +0300

    fixed many .I.

--- WebContent/resources/js/google_places_autocomp.js
+++ WebContent/resources/js/google_places_autocomp.js
@@ -8,19 +8,19 @@
 
 var placeSearch, autocomplete;
 var componentForm = {
-    //street_number: 'short_name',
-    //route: 'long_name',
-    //locality: 'long_name',
+    street_number: 'short_name',
+    route: 'long_name',
+    locality: 'long_name',
     //administrative_area_level_1: 'short_name',
     country: 'long_name',
-    //postal_code: 'short_name'
+    postal_code: 'short_name'
 };
 
 function initAutocomplete() {
 // Create the autocomplete object, restricting the search to geographical
 // location types.
     autocomplete = new google.maps.places.Autocomplete(
-    /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+    /** @type {!HTMLInputElement} */(document.getElementById('locSearch:mainstr')),
         {types: ['geocode']});
 
 // When the user selects an address from the dropdown, populate the address
@@ -31,8 +31,8 @@ function initAutocomplete() {
 function fillInAddress() {
 // Get the place details from the autocomplete object.
     var place = autocomplete.getPlace();
-    //document.getElementById("locSearch:country").value = "lalaal";
     for(var component in componentForm) {
+    	// Add jsf form id
     	plusformid = "locSearch:" + component;
         document.getElementById(plusformid).value = '';
         document.getElementById(plusformid).disabled = false;
--- src/bean/MainBean.java
+++ src/bean/MainBean.java
@@ -1,20 +1,18 @@
 package bean;
-
 import model.Listing;import model.Location;import java.util.List;import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.persistence.Column;import dao.MainDAO;
-
 @ManagedBean(name="main", eager=true)
 @SessionScoped
 public class MainBean {
-  private String country;  private String out_str;    @ManagedProperty(value="#{mainDAO}")  private MainDAO mainDAO;
+  private String mainstr;  private String country;  private String locality;  private String route;  private String street_number;  private String postal_code;  private String out_str;    @ManagedProperty(value="#{mainDAO}")  private MainDAO mainDAO;  public void setMainstr(String mainstr) {	  this.mainstr = mainstr;  }  public String getMainstr() {	  return mainstr;  }  
   public void setCountry(String country) {
 	  this.country = country;
   }
   public String getCountry() {
 	  return country;
-  }    public void setOut_str(String out_str) {	  this.out_str = out_str;  }  public String getOut_str() {	  return out_str;  }
+  }    public void setLocality(String locality) {	  this.locality = locality;  }  public String getLocality() {	  return locality;  }  public void setRoute(String route) {	  this.route = route;  }  public String getRoute() {	  return route;  }  public void setStreet_number(String street_number) {	  this.street_number = street_number;  }  public String getStreet_number() {	  return street_number;  }  public void setPostal_code(String postal_code) {	  this.postal_code = postal_code;  }  public String getPostal_code() {	  return postal_code;  }  public void setOut_str(String out_str) {	  this.out_str = out_str;  }  public String getOut_str() {	  return out_str;  }
   public void setMainDAO(MainDAO mainDAO) {	  this.mainDAO = mainDAO;  }	  public MainDAO getMainDAO() {	  return mainDAO;  }
-  public String find_country() {	  Location curr = mainDAO.find(country);	  out_str = curr.getCountry() + "  xaxaxaxa   " + curr.getStreet();	  /*if (curr.getCountry() == null)			return "error";		else			return curr.getCountry();*/	  return "/index?faces-redirect=true";  }
+  public String find_country() {	  //Location curr = mainDAO.find(country);	  out_str = mainstr;	  	  /*if (curr.getCountry() == null)			return "error";		else			return curr.getCountry();*/	  return "/index?faces-redirect=true";  }
 } 
