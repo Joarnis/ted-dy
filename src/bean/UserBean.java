7fe5703b6e32de4fb8de2c2b8d98da7e02d1537e
konstantinoscs <kons.ste@gmail.com>
2017-08-29 21:14:38 +0300

    continuing for the registration and changes to general css

--- src/bean/UserBean.java
+++ src/bean/UserBean.java
@@ -28,12 +28,44 @@ public class UserBean {
 	@ManagedProperty(value="#{userDAO}")
     private UserDAO userDAO;
 	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setIsHost(byte isHost) {
+		this.isHost = isHost;
+	}
+	
+	public byte getIsHost() {
+		return isHost;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	
 	public String getPassword() {
-		return this.password;
+		return password;
 	}
 	
 	public void setPhoneNum(String phoneNum) {
